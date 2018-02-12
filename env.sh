2bafeb575f06183064257cfc0609d45183f5073a
Tiffany <tifchang@wharton.upenn.edu>
2018-02-12 17:35:52 -0500

    done

---
+++ env.sh
@@ -0,0 +1,3 @@
+PWD='email1slyfewh0evenu8esfb'
+
+mongodb://pennlabs:email1slyfewh0evenu8esfb@ds155080.mlab.com:55080/penncourselmk
--- pcr.js
+++ pcr.js
@@ -19,25 +19,42 @@ function write(input) {
  })
 }
 
+// Reading PCA data
+fs.readFile('./courses.json', "utf8", (err, data) => {
+  if (err) throw err;
+  var pca = JSON.parse(data);
+  var coursecode = Object.keys(pca).map((k) => {
+    // console.log(pca[k].course);
+    return '2016a-' + pca[k].course;
+  });
+  setTimeout(function() {
+    pull_pcrdata(coursecode);
+  }, 5000);
+  // console.log(coursecode.length);
+});
+
 // Courses to retrieve data on
-var coursecode = ['2016c-CIS-120', '2016a-STAT-476'];
 var allcourses = {};
 // Get reviews for each course
-coursecode.forEach(e => {
-  request('http://api.penncoursereview.com/v1/courses/'+ e +'/reviews?token=KBfHZ21jf6e0bED2FQRkx_Ex42NvEv',
-  function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      console.log(JSON.parse(body).result.values[0].id)
-      var id = JSON.parse(body).result.values[0].id;
-      allcourses[id] = JSON.parse(body).result.values[0];
-    }
+function pull_pcrdata(coursecode) {
+  coursecode.forEach(e => {
+    request('http://api.penncoursereview.com/v1/courses/'+ e +'/reviews?token=KBfHZ21jf6e0bED2FQRkx_Ex42NvEv',
+    function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        // console.log(JSON.parse(body).result.values[0].id)
+        var id = JSON.parse(body).result.values[0].id;
+        allcourses[id] = JSON.parse(body).result.values[0];
+      }
+    });
   });
-});
+}
+
 
 // Write out to file after coursecode is complete
 setTimeout(function() {
   write(allcourses);
-}, 5000)
+  // console.log("DONE");
+}, 120000)
 
 app.listen(3000);
 console.log("Started");
